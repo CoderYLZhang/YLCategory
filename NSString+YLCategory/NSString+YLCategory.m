4c10890fc913a3c65cf57fb1cb4b4dfb224b4ca8
张银龙 <469891897@qq.com>
2016-01-05 21:02:01 +0800

    添加分类

--- NSString+YLCategory/NSString+YLCategory.m
+++ NSString+YLCategory/NSString+YLCategory.m
@@ -2,8 +2,8 @@
 //  NSString+YLCategory.m
 //  自定义cell
 //
-//  Created by 张银龙 on 13/12/8.
-//  Copyright © 2013年 张银龙. All rights reserved.
+//  Created by 张银龙 on 14/12/8.
+//  Copyright © 2014年 张银龙. All rights reserved.
 //
 
 #import "NSString+YLCategory.h"
---
+++ UIView+Frame/UIView+Frame.h
@@ -0,0 +1,16 @@
+//
+//  UIView+Frame.h
+//  
+//
+//  Created by 张银龙 on 14/12/8.
+//  Copyright © 2014年 张银龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@end
