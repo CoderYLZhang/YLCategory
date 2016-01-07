f47d938f297bd5afbcd199935cecc469c5f2686a
张银龙 <469891897@qq.com>
2016-01-07 22:12:06 +0800

    更新分类

---
+++ YLButton/YLButton.m
@@ -0,0 +1,33 @@
+//
+//  YLButton.m
+//  
+//
+//  Created by 张银龙 on 14/12/8.
+//  Copyright © 2014年 张银龙. All rights reserved.
+//
+
+#import "YLButton.h"
+
+@implementation YLButton
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    CGFloat imageW = self.frame.size.width;
+    CGFloat imageH = imageW;
+    self.imageView.frame = CGRectMake(0, 0, imageW, imageH);
+    CGFloat titleW = imageW;
+    CGFloat titleH = self.frame.size.height - imageH;
+    CGFloat titleY = imageH;
+    self.titleLabel.frame = CGRectMake(0, titleY, titleW, titleH);
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    }
+    return self;
+}
+
+@end
