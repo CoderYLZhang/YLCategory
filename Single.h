88a90244ba23cacdd68daa5cc66ce037307ef81f
张银龙 <469891897@qq.com>
2016-01-11 16:16:38 +0800

    添加单例

---
+++ Single.h
@@ -0,0 +1,76 @@
+//
+//  Single.h
+//  05-掌握-单例模式（通用）
+//
+//  Created by xiaomage on 16/1/11.
+//  Copyright © 2016年 xiaomage. All rights reserved.
+//
+
+#define SingleH(name) +(instancetype)share##name;
+
+#if  __has_feature(objc_arc)
+//ARC
+#define SingleM(name) static id _instance;\
++(instancetype)allocWithZone:(struct _NSZone *)zone\
+{\
+static dispatch_once_t onceToken;\
+dispatch_once(&onceToken, ^{\
+_instance = [super allocWithZone:zone];\
+});\
+return _instance;\
+}\
+\
++(instancetype)share##name\
+{\
+return [[self alloc]init];\
+}\
+\
+-(id)copyWithZone:(NSZone *)zone\
+{\
+return _instance;\
+}\
+\
+-(id)mutableCopyWithZone:(NSZone *)zone\
+{\
+return _instance;\
+}
+#else
+//mrc
+#define SingleM(name) static id _instance;\
++(instancetype)allocWithZone:(struct _NSZone *)zone\
+{\
+static dispatch_once_t onceToken;\
+dispatch_once(&onceToken, ^{\
+_instance = [super allocWithZone:zone];\
+});\
+return _instance;\
+}\
+\
++(instancetype)share##name\
+{\
+return [[self alloc]init];\
+}\
+\
+-(id)copyWithZone:(NSZone *)zone\
+{\
+return _instance;\
+}\
+\
+-(id)mutableCopyWithZone:(NSZone *)zone\
+{\
+return _instance;\
+}\
+-(oneway void)release\
+{\
+}\
+\
+-(instancetype)retain\
+{\
+    return _instance;\
+}\
+\
+-(NSUInteger)retainCount\
+{\
+    return MAXFLOAT;\
+}
+#endif
