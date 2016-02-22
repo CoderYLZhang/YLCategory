2980ece970af67b91acb4cb4decaab86d650fdc7
张银龙 <469891897@qq.com>
2016-02-22 08:48:40 +0800

    添加之类

---
+++ YLButton/YLSquareButton.h
@@ -0,0 +1,13 @@
+//
+//  YLSquareButton.h
+//
+//
+//  Created by xiaomage on 14/9/7.
+//  Copyright (c) 2014年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YLSquareButton : UIButton
+
+@end
---
+++ YLButton/YLSquareButton.m
@@ -0,0 +1,39 @@
+//
+//  YLSquareButton.m
+//
+//
+//  Created by xiaomage on 14/9/7.
+//  Copyright (c) 2014年 xiaomage. All rights reserved.
+//
+
+#import "YLSquareButton.h"
+
+
+@implementation YLSquareButton
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        self.titleLabel.textAlignment = NSTextAlignmentCenter;
+        self.titleLabel.font = [UIFont systemFontOfSize:14];
+        [self setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    }
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    self.imageView.width = self.width * 0.5;
+    self.imageView.height = self.imageView.width;
+    self.imageView.y = self.height * 0.1;
+    self.imageView.centerX = self.width * 0.5;
+    
+    self.titleLabel.width = self.width;
+    self.titleLabel.y = CGRectGetMaxY(self.imageView.frame);
+    self.titleLabel.x = 0;
+    self.titleLabel.height = self.height - self.titleLabel.y;
+}
+
+@end
