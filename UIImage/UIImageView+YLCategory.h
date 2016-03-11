6176dd72d5e8ba4dc0cca4900075402a07eea4a1
张银龙 <469891897@qq.com>
2016-03-11 16:16:13 +0800

    添加

---
+++ UIImage/UIImageView+YLCategory.h
@@ -0,0 +1,16 @@
+//
+//  UIImageView+YLCategory.h
+//  BuDeJie_first
+//
+//  Created by 张银龙 on 14/12/8.
+//  Copyright © 2014年 张银龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (YLCategory)
+
+- (void)setImageBDImage:(NSString*)BDImage andNormalImage:(NSString *)normalImage placholder:(UIImage *)placholder;
+
+
+@end
